cc52e397782b2c32825374a968822cf346c9ef5f
vvinitski-idt <viktar.vinitski@idt.net>
2024-03-02 16:55:46 +0100

    feature: added main handler

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { graphql, parse, validate } from 'graphql';
+import rootSchema from './graphql-schemas/root-schema.js';
+import depthLimit from 'graphql-depth-limit';
+import getDataLoaders from './dataloaders/dataloaders.js';
+
+const GRAPHQL_DEPTH_LIMIT = 5;
+const QUERY_VALIDATING_ERROR_MESSAGE = 'Error validating request query!';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,7 +19,36 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const {
+        body: { query, variables },
+      } = req;
+
+      const validateErrors = validate(rootSchema, parse(query), [
+        depthLimit(GRAPHQL_DEPTH_LIMIT),
+      ]);
+
+      if (validateErrors.length) {
+        return {
+          data: {
+            message: QUERY_VALIDATING_ERROR_MESSAGE,
+          },
+          errors: validateErrors,
+        };
+      }
+
+      const contextValue = {
+        prismaClient: fastify.prisma,
+        dataloaders: getDataLoaders(fastify.prisma),
+      };
+
+      const { data, errors } = await graphql({
+        schema: rootSchema,
+        source: query,
+        variableValues: variables,
+        contextValue,
+      });
+
+      return { data, errors };
     },
   });
 };
